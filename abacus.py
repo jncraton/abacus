338a9a74af5f1fa15ba016af8cb42ce673911bef
Jon Craton <jncraton@gmail.com>
2026-01-22 09:42:44 -0500

    Add abacus.py

---
+++ abacus.py
@@ -0,0 +1,32 @@
+class Rod:
+    def __init__(self, upperBeadsUp=1, lowerBeadsUp=0):
+        self.upperBeadsUp = upperBeadsUp
+        self.lowerBeadsUp = lowerBeadsUp
+
+    def __repr__(self):
+        return str((self.upperBeadsUp, self.lowerBeadsUp))
+
+class Abacus:
+    """
+    >>> Abacus()
+    [(1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0)]
+    
+    >>> Abacus([Rod(1, 1)]).rods[-1]
+    (1, 1)
+    """
+    def __init__(self, rods = []):
+        # 9 Rods from left (0) to right (8)
+        self.rods = [Rod() for _ in range(9)]
+
+        # Set any provided rods pushing to the right
+        for i, rod in enumerate(rods[::-1]):
+            self.rods[-(i+1)] = rod
+
+    def __repr__(self):
+        return str(self.rods)
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
